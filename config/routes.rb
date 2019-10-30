178a3cb6ce67feb8c649014e1e3fb8de0e890194
Gonzalo Diaz <info@ventra.com.co>
2019-10-30 10:06:14 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
    get '/zanzibar', to: 'pages#zanzibar'
+   get 'download_pdf', to: "pages#download_pdf"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
